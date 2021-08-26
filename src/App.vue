246767c08a8a59c050ddbc78708e8744e99bb3f9
Innah-love-web <innatally@outlook.com>
2021-08-26 23:51:19 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app dark>
+      <div class="d-flex align-center mr-2">
+        Student Management System
+      </div>
+
+      <v-btn to="/students" text>
+        Students
+      </v-btn>
+
+      <v-btn to="/add" text>
+        Add Students
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
